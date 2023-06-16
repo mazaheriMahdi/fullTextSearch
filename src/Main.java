8afa9f25ca726723a569be9bee8831367e733fc9
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-06-16 14:19:57 +0330

    refactor code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<String> stopWords = List.of(
+    static final List<String> stopWords = List.of(
             "&", "a", "an", "the", "in", "on", "at",
             "to", "is", "are", "am", "was", "were",
             "be", "been", "being", "have", "has",
@@ -30,48 +30,81 @@ public class Main {
         Map<String, LinkedList<String>> data = new HashMap<>();
         for (File file : files) {
             try {
-
-                Scanner reader = new Scanner(file);
-                StringBuilder content = new StringBuilder();
-                while (reader.hasNextLine()) {
-                    content.append(reader.nextLine());
-                }
-
-                LinkedList<String> words = new LinkedList<>(Arrays.asList(content.toString().toUpperCase().split("\\s|'|\\.|!|\"|\"|,|:|;|\\?|\\(|\\)|\\[|\\]|\\{|\\}|\\*|/|\\\\")));
-                data.put(file.getName(), words);
+                getFilesWords(data, file);
             } catch (Exception e) {
                 System.out.println("File not found");
             }
         }
-
         LinkedList<String> tokens = new LinkedList<>();
         for (String docs : data.keySet()) {
-
             tokens.addAll(data.get(docs));
         }
+        startSearching(scanner, data, tokens);
+
+    }
+
+    private static void startSearching(Scanner scanner, Map<String, LinkedList<String>> data, LinkedList<String> tokens) {
+        Map<String, LinkedList<String>> invertedIndex = getStringLinkedListMap(data, tokens);
+        writeDataToFile(invertedIndex);
+        while (true) {
+            System.out.println("Enter word to search: ");
+            String searchPhrase = scanner.nextLine();
+            LinkedList<String> result = search(invertedIndex, searchPhrase);
+            System.out.println(result);
+        }
+    }
+
+    private static LinkedList<String> search(Map<String, LinkedList<String>> invertedIndex, String search) {
+        LinkedList<String> result = new LinkedList<>();
+        LinkedList<String> plus = new LinkedList<>();
+        LinkedList<String> minus = new LinkedList<>();
+
+        for (String s : search.split(" ")) {
+            if (invertedIndex.containsKey(s.toUpperCase())){
+                if (s.startsWith("+"))
+                    plus.add(s.substring(1).toUpperCase());
+                else if (s.startsWith("-"))
+                    minus.add(s.substring(1).toUpperCase());
+                else
+                    if (result.isEmpty())result.addAll(invertedIndex.get(s.toUpperCase()));
+                    else result.retainAll(invertedIndex.get(s.toUpperCase()));
+            }
+        }
+        if (!plus.isEmpty()){
+            result.retainAll(plus);
+            result.removeAll(minus);
+        }
+        return result;
+    }
+
+    private static void writeDataToFile(Map<String, LinkedList<String>> invertedIndex) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("data.txt"));
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(invertedIndex);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Map<String, LinkedList<String>> getStringLinkedListMap(
+            Map<String, LinkedList<String>> data,
+            LinkedList<String> tokens) {
 
         Map<String, LinkedList<String>> invertedIndex = new HashMap<>();
         final Integer[] index = {tokens.size()};
-
-
         ArrayList<Thread> threads = new ArrayList<>();
         for (String word : tokens) {
-            if (stopWords.contains(word)) {
-                continue;
-            }
-
+            if (stopWords.contains(word)) continue;
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     System.out.print("\033[H\033[2J");
                     System.out.flush();
                     System.out.println("Processing word: " + index[0]-- + "/" + tokens.size());
-                    LinkedList<String> docs = new LinkedList<>();
-                    for (String docsName : data.keySet()) {
-                        if (data.get(docsName).contains(word)) {
-                            docs.add(docsName);
-                        }
-                    }
+                    LinkedList<String> docs = getDocs(data, word);
                     invertedIndex.put(word, docs);
                 }
             });
@@ -86,43 +119,28 @@ public class Main {
                 e.printStackTrace();
             }
         });
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream(new File("data.txt"));
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            objectOutputStream.writeObject(invertedIndex);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        return invertedIndex;
+    }
+
+    private static LinkedList<String> getDocs(Map<String, LinkedList<String>> data, String word) {
+        LinkedList<String> docs = new LinkedList<>();
+        for (String docsName : data.keySet()) {
+            if (data.get(docsName).contains(word)) {
+                docs.add(docsName);
+            }
         }
-        while (true) {
-            System.out.println("Enter word to search: ");
-            String search = scanner.nextLine();
-            LinkedList<String> result = new LinkedList<>();
+        return docs;
+    }
 
-            LinkedList<String> plus = new LinkedList<>();
-            LinkedList<String> minus = new LinkedList<>();
-
-            for (String s : search.split(" ")) {
-                if (invertedIndex.containsKey(s.toUpperCase())){
-                    if (s.startsWith("+"))
-                        plus.add(s.substring(1).toUpperCase());
-                    else if (s.startsWith("-"))
-                        minus.add(s.substring(1).toUpperCase());
-                    else
-                        if (result.isEmpty())result.addAll(invertedIndex.get(s.toUpperCase()));
-                        else result.retainAll(invertedIndex.get(s.toUpperCase()));
-                }
-            }
-            if (!plus.isEmpty()){
-                result.retainAll(plus);
-                result.removeAll(minus);
-            }
-
-            System.out.println(result);
+    private static void getFilesWords(Map<String, LinkedList<String>> data, File file) throws FileNotFoundException {
+        Scanner reader = new Scanner(file);
+        StringBuilder content = new StringBuilder();
+        while (reader.hasNextLine()) {
+            content.append(reader.nextLine());
         }
 
-//        System.out.println(invertedIndex);
+        LinkedList<String> words = new LinkedList<>(Arrays.asList(content.toString().toUpperCase().split("\\s|'|\\.|!|\"|\"|,|:|;|\\?|\\(|\\)|\\[|\\]|\\{|\\}|\\*|/|\\\\")));
+        data.put(file.getName(), words);
     }
 
 }
